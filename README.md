36b9f0bd855d120d97958077f2226457dc95d080
Dylan Steele <dylansteele8@gmail.com>
2015-09-04 21:34:11 -0400

    Add mongodb mongdb connection

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # pennapps_f15
 
+Call (304) BLING-17
+
 Twilio
 MongoDB
 SoundCloud/Last.fm/UberStation
--- run.py
+++ run.py
@@ -1,13 +1,49 @@
 from flask import Flask
 import twilio.twiml
+from pymongo import MongoClient
  
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
+    # Get the caller's phone number from the incoming Twilio request
+    from_number = request.values.get('From', None)
+
+    client = MongoClient()
+    db = client.test
+
+    result = db.test.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
+    
+    # Respond to incoming requests
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    if from_number in db.test.find():
+        # Greet the caller by name
+        resp.say("Hello " + callers[from_number])
+    else:
+        resp.say("Hello Monkey")
  
     return str(resp)
  
