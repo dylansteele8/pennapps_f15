2d63bccd86b38cf9f804f0d665fd52b47b1bef9d
Dylan Steele <dylansteele8@gmail.com>
2015-09-04 22:16:58 -0400

    Find accphone accounts

--- run.py
+++ run.py
@@ -10,42 +10,46 @@ def hello_monkey():
     from_number = request.values.get('From', None)
 
     client = MongoClient()
-    db = client.test
-
-    result = db.test.insert_one(
-    {
-        "address": {
-            "street": "2 Avenue",
-            "zipcode": "10075",
-            "building": "1480",
-            "coord": [-73.9557413, 40.7720266]
-        },
-        "borough": "Manhattan",
-        "cuisine": "Italian",
-        "grades": [
-            {
-                "grade": "A",
-                "score": 11
-            },
-            {
-                "grade": "B",
-                "score": 17
-            }
-        ],
-        "name": "Vella",
-        "restaurant_id": "41704620"
-    }
-)
-    
-    # Respond to incoming requests
+    db = client.accounts
     resp = twilio.twiml.Response()
-    if from_number in db.test.find():
-        # Greet the caller by name
-        resp.say("Hello " + callers[from_number])
+
+    # Respond to incoming requests
+    if db.accounts.find({"phone_number": from_number }):
+        resp.say("Welcome back")
+        resp.say(resume_play(from_number))
     else:
-        resp.say("Hello Monkey")
- 
+        resp.say("Let's begin")
+        result = db.test.insert_one(
+            {
+                "phone_number": from_number
+            }
+        )
+        resp.say(start_play())
+
     return str(resp)
  
 if __name__ == "__main__":
     app.run(debug=True)
+
+# {
+#     "address": {
+#         "street": "2 Avenue",
+#         "zipcode": "10075",
+#         "building": "1480",
+#         "coord": [-73.9557413, 40.7720266]
+#     },
+#     "borough": "Manhattan",
+#     "cuisine": "Italian",
+#     "grades": [
+#         {
+#             "grade": "A",
+#             "score": 11
+#         },
+#         {
+#             "grade": "B",
+#             "score": 17
+#         }
+#     ],
+#     "name": "Vella",
+#     "restaurant_id": "41704620"
+# }
